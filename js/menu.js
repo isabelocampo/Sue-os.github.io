95622b6aa0315012b3729c6055b59b06d714b62a
isabelocampo <143030333+isabelocampo@users.noreply.github.com>
2023-08-23 15:15:35 -0500

    initial commit

---
+++ js/menu.js
@@ -0,0 +1,18 @@
+const hamburguer = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu-navegacion');
+
+hamburguer.addEventListener('click', ()=>{
+    menu.classList.toggle("spread")
+})
+
+window.addEventListener('click', e=>{
+
+})
+
+window.addEventListener('click', e=>{
+    if(menu.classList('spread')&& e.target != menu && e.target != hamburguer){
+        menu.classList.tooggle('sptread')
+        
+    }
+})
+
